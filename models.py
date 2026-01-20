fa1ecf13b83b5d1d65ed5204076f9a4a8527aaf3
123oiler05 <123oiler05@gmail.com>
2026-01-20 02:10:36 -0600

    Initial commit

---
+++ models.py
@@ -0,0 +1,20 @@
+from extensions import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    apellido = db.Column(db.String(100), nullable=False)
+    edad = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    nombre_usuario = db.Column(db.String(80), nullable=False, unique=True)
+    contrasena = db.Column(db.String(255), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "apellido": self.apellido,
+            "edad": self.edad,
+            "email": self.email,
+            "nombre_usuario": self.nombre_usuario
+        }
